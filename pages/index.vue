8cd490ad4a3f92649dbd45ecfdf235f4a9bec9ff
osawa-koki <github@osawa.dev>
2022-12-29 12:55:46 +0900

    fst commit;;

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <img :src="$router.options.base + 'tako.png'" alt="tako" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>
