370b9b7232e458196d6e17cc1771d4b84335393e
osawa-koki <koki@osawa.dev>
2024-01-08 00:28:38 +0900

    リセットアップ。

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,6 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    'jest/globals': true,
     node: true
   },
   extends: [
@@ -10,7 +9,6 @@ module.exports = {
     'plugin:nuxt/recommended'
   ],
   plugins: [
-    'jest'
   ],
   // add your custom rules here
   rules: {}
